f6a70525d727fade336d19f49123ffd2e82ac6de
Evans Rutto <robbyevans001@gmail.com>
2022-10-19 13:30:19 +0300

    added styling

--- client/src/ClerkPage.js
+++ client/src/ClerkPage.js
@@ -14,6 +14,7 @@ const initialState = {
 
 function ClerkPage() {
   const[items,setItems]=useState(initialState)
+  const[isTrue,setIsTrue]=useState(true)
 
   function handleChange(e) {
     setItems({
@@ -39,67 +40,75 @@ function ClerkPage() {
       });
   }
 
-
+  function handleClick(){
+    setIsTrue(!isTrue)
+  }
 
   return (
-    <div className="clerk-input">
-    <h2>Add Items</h2>
-    <form className="item-form" onSubmit={handleSubmit}>
-      
-      <input
-        type="text"
-        id="name"
-        value={items.name}
-        onChange={handleChange}
-        placeholder="name"
-      />
-    <input
-      type="text"
-      id="quantity"
-      value={items.quantity}
-      onChange={handleChange}
-      placeholder="quantity"
-    />
-      <input
-        type="number"
-        id="inStock"
-        value={items.inStock}
-        onChange={handleChange}
-        placeholder="items in stock"
-      />
-      <input
-        type="number"
-        id="spoilt"
-        value={items.spoilt}
-        onChange={handleChange}
-        placeholder="items spoilt"
-      />
+      <div class="clerk-container">
+        <div class="clerk-navbar">
+          <button className="add-btn" onClick={handleClick}> Add Items +</button>
+          <button className="add-btn" onClick={handleClick}> Add Items +</button>
+          </div>
 
-      <input
-        type="number"
-        id="buyingPrice"
-        value={items.buyingPrice}
-        onChange={handleChange}
-        placeholder="buying price"
-      />
-
-      <input
-        type="number"
-        id="sellingPrice"
-        value={items.sellingPrice}
-        onChange={handleChange}
-        placeholder="selling price"
-      />
-      <input
+        {isTrue ?
+            <div className="clerk-input">
+            <h2>Add Items</h2>
+            <form className="item-form" onSubmit={handleSubmit}>
+        
+        <input
+          type="text"
+          id="name"
+          value={items.name}
+          onChange={handleChange}
+          placeholder="name"
+        />
+            <input
         type="text"
-        id="status"
-        value={items.status}
+        id="quantity"
+        value={items.quantity}
         onChange={handleChange}
-        placeholder="payment status"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+        placeholder="quantity"
+            />
+        <input
+          type="number"
+          id="inStock"
+          value={items.inStock}
+          onChange={handleChange}
+          placeholder="items in stock"
+        />
+        <input
+          type="number"
+          id="spoilt"
+          value={items.spoilt}
+          onChange={handleChange}
+          placeholder="items spoilt"
+        />
+        <input
+          type="number"
+          id="buyingPrice"
+          value={items.buyingPrice}
+          onChange={handleChange}
+          placeholder="buying price"
+        />
+        <input
+          type="number"
+          id="sellingPrice"
+          value={items.sellingPrice}
+          onChange={handleChange}
+          placeholder="selling price"
+        />
+        <input
+          type="text"
+          id="status"
+          value={items.status}
+          onChange={handleChange}
+          placeholder="payment status"
+        />
+        <button type="submit">Submit</button>
+            </form>
+          </div>:null}
+      </div>
   )
 }
 
