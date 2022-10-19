3d8db64400bc2c326a54c8dde2fd07833c3fc3a1
Evans Rutto <robbyevans001@gmail.com>
2022-10-19 11:31:30 +0300

    initial commit

---
+++ client/src/ClerkPage.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import {useState,useEffect} from "react"
+
+const initialState = {
+  name: "",
+  quantity: "",
+  inStock: "",
+  spoilt: "",
+  buyingPrice: "",
+  sellingPrice: "",
+  status:""
+};
+
+
+function ClerkPage() {
+  const[items,setItems]=useState(initialState)
+
+  function handleChange(e) {
+    setItems({
+      ...items,
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(items),
+      
+    })
+      .then((r) => r.json())
+      .then((newItem) => {
+        setItems(initialState);
+        // onAddItem(newItem);
+      });
+  }
+
+
+
+  return (
+    <div className="input-form ">
+    <h2>Add Event/Activity</h2>
+    <form className="new_event" onSubmit={handleSubmit}>
+      
+      <input
+        type="text"
+        id="name"
+        value={items.name}
+        onChange={handleChange}
+        placeholder="name"
+      />
+    <input
+      type="text"
+      id="quantity"
+      value={items.quantity}
+      onChange={handleChange}
+      placeholder="quantity"
+    />
+      <input
+        type="number"
+        id="inStock"
+        value={items.inStock}
+        onChange={handleChange}
+        placeholder="items in stock"
+      />
+      <input
+        type="number"
+        id="spoilt"
+        value={items.spoilt}
+        onChange={handleChange}
+        placeholder="items spoilt"
+      />
+
+      <input
+        type="number"
+        id="buyingPrice"
+        value={items.buyingPrice}
+        onChange={handleChange}
+        placeholder="buying price"
+      />
+
+      <input
+        type="number"
+        id="sellingPrice"
+        value={items.sellingPrice}
+        onChange={handleChange}
+        placeholder="selling price"
+      />
+      <input
+        type="text"
+        id="status"
+        value={items.status}
+        onChange={handleChange}
+        placeholder="payment status"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+  )
+}
+
+export default ClerkPage
