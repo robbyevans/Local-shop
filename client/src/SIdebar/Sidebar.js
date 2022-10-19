aa85c88a2f2fea3ca9cfa43def9b07a39b33b2bc
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-19 23:04:07 +0300

    add sidebar component

--- client/src/SIdebar/Sidebar.js
+++ client/src/SIdebar/Sidebar.js
@@ -45,9 +45,25 @@ const Sidebar = () => {
     ]
 
     return (
-        <div>
-            
+        <div className="container">
+    <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
+        <div className="top_section">
+            <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Local Shop</h1>
+            <div style={{marginLeft: isOpen ? "70px" : "0px"}} className="bars">
+                <FaBars onClick={toggle}/>
+            </div>
         </div>
+        {
+            menuItem.map((item, index)=>(
+                <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                    <div className="icon">{item.icon}</div>
+                    <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                </NavLink>
+            ))
+        }
+    </div>
+ <main>{children}</main>
+ </div>
     );
 }
 
