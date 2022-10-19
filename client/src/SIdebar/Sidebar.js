ed4cbb5fc28d2d7c48fe69848c84c3e0c67f6e17
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-19 23:01:00 +0300

    add icons

---
+++ client/src/SIdebar/Sidebar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Sidebar = () => {
+
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+    const menuItem=[
+        {
+            path:"/",
+            name:"Dashboard",
+            icon:<FaTh/>
+        },
+        {
+            path:"/about",
+            name:"About",
+            icon:<FaUserAlt/>
+        },
+        {
+            path:"/analytics",
+            name:"Analytics",
+            icon:<FaRegChartBar/>
+        },
+        {
+            path:"/comment",
+            name:"Comment",
+            icon:<FaCommentAlt/>
+        },
+        {
+            path:"/product",
+            name:"Product",
+            icon:<FaShoppingBag/>
+        },
+        {
+            path:"/productList",
+            name:"Product List",
+            icon:<FaThList/>
+        }
+    ]
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Sidebar;
