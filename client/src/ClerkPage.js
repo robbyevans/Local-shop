21d376537edf116336c6935ea94bd45268dd5b46
Evans Rutto <robbyevans001@gmail.com>
2022-10-19 16:41:30 +0300

    created table

--- client/src/ClerkPage.js
+++ client/src/ClerkPage.js
@@ -14,7 +14,8 @@ const initialState = {
 
 function ClerkPage() {
   const[items,setItems]=useState(initialState)
-  const[isTrue,setIsTrue]=useState(true)
+  const[isTrue,setIsTrue]=useState(null)
+  const[request,setRequest]=useState(null)
 
   function handleChange(e) {
     setItems({
@@ -41,74 +42,120 @@ function ClerkPage() {
   }
 
   function handleClick(){
+    setRequest(null)
     setIsTrue(!isTrue)
+  }
+  function handleRequest(){
+    setIsTrue(null)
+    setRequest(!request)
   }
 
   return (
-      <div class="clerk-container">
-        <div class="clerk-navbar">
-          <button className="add-btn" onClick={handleClick}> Add Items +</button>
-          <button className="add-btn" onClick={handleClick}> Add Items +</button>
-          </div>
+        <div className="clerk">
+          <div className="clerk-banner"><h1 className='clerk-title'>Clerk Page</h1></div>
+                <div class="clerk-container">
+          <div class="clerk-navbar">
+            <button className="add-btn" onClick={handleClick}> Add Items +</button>
+            <button className="add-btn" onClick={handleRequest}> Request Items +</button>
+            </div>
+          {isTrue ?
+              <div className="clerk-input">
+              <h2>Add Items</h2>
+              <form className="item-form" onSubmit={handleSubmit}>
+          
+          <input
+            type="text"
+            id="name"
+            value={items.name}
+            onChange={handleChange}
+            placeholder="name"
+          />
+              <input
+          type="text"
+          id="quantity"
+          value={items.quantity}
+          onChange={handleChange}
+          placeholder="quantity"
+              />
+          <input
+            type="number"
+            id="inStock"
+            value={items.inStock}
+            onChange={handleChange}
+            placeholder="items in stock"
+          />
+          <input
+            type="number"
+            id="spoilt"
+            value={items.spoilt}
+            onChange={handleChange}
+            placeholder="items spoilt"
+          />
+          <input
+            type="number"
+            id="buyingPrice"
+            value={items.buyingPrice}
+            onChange={handleChange}
+            placeholder="buying price"
+          />
+          <input
+            type="number"
+            id="sellingPrice"
+            value={items.sellingPrice}
+            onChange={handleChange}
+            placeholder="selling price"
+          />
+          <input
+            type="text"
+            id="status"
+            value={items.status}
+            onChange={handleChange}
+            placeholder="payment status"
+          />
+          <button type="submit">Submit</button>
+              </form>
+            </div>:null}
+            {request ?
+              <div className="clerk-input">
+              <h2>Request Item</h2>
+              <form className="item-form" onSubmit={handleSubmit}>
+          
+          <input
+            type="text"
+            id="ClerkName"
+            value={items.name}
+            onChange={handleChange}
+            placeholder="Clerk Name"
+          />
+              <input
+          type="text"
+          id="Item"
+          value={items.Item}
+          onChange={handleChange}
+          placeholder="Item"
+              />
+          <input
+            type="number"
+            id="Quantity"
+            value={items.quantity}
+            onChange={handleChange}
+            placeholder="Quantity"
+          />
+          <input
+            type="date"
+            id="date"
+            value={items.date}
+            onChange={handleChange}
+            placeholder="Due Date"
+          />
+          <button type="submit">Submit</button>
+              </form>
+            </div>:null}
+                </div>
 
-        {isTrue ?
-            <div className="clerk-input">
-            <h2>Add Items</h2>
-            <form className="item-form" onSubmit={handleSubmit}>
-        
-        <input
-          type="text"
-          id="name"
-          value={items.name}
-          onChange={handleChange}
-          placeholder="name"
-        />
-            <input
-        type="text"
-        id="quantity"
-        value={items.quantity}
-        onChange={handleChange}
-        placeholder="quantity"
-            />
-        <input
-          type="number"
-          id="inStock"
-          value={items.inStock}
-          onChange={handleChange}
-          placeholder="items in stock"
-        />
-        <input
-          type="number"
-          id="spoilt"
-          value={items.spoilt}
-          onChange={handleChange}
-          placeholder="items spoilt"
-        />
-        <input
-          type="number"
-          id="buyingPrice"
-          value={items.buyingPrice}
-          onChange={handleChange}
-          placeholder="buying price"
-        />
-        <input
-          type="number"
-          id="sellingPrice"
-          value={items.sellingPrice}
-          onChange={handleChange}
-          placeholder="selling price"
-        />
-        <input
-          type="text"
-          id="status"
-          value={items.status}
-          onChange={handleChange}
-          placeholder="payment status"
-        />
-        <button type="submit">Submit</button>
-            </form>
-          </div>:null}
-      </div>
+                <h1>Available Items</h1>
+                
+        </div>
   )
 }
 
