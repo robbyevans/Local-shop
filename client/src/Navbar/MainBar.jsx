30ea1652ab09af244d0d5dc72d6039ed592c33b0
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-20 22:35:47 +0300

    add ordered list

--- client/src/Navbar/MainBar.jsx
+++ client/src/Navbar/MainBar.jsx
@@ -7,7 +7,12 @@ function MainBar() {
          <div className='menu-icon' onClick={handleClick}>
             <i className={click ? "fas fa-times" : " fas fa-bars"}></i>
           </div>
+   
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
 
+
+            
+          </ul>
          </div>
     </nav>
   )
