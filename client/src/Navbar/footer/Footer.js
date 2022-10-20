f3b183232745607a9616ccde0c298e02ad988ef7
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-20 23:03:45 +0300

    add footer component

---
+++ client/src/Navbar/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Footer.css"
+
+
+const Footer = () => {
+  return (
+      <div className='legal'>
+        <p>LocalShop © 2022 All Rights Reserved.</p>
+      </div>
+    
+  )
+}
+
+export default Footer
