0f9ddfa274c5c09f07a65bd5c4c5a6d95a71e994
Peter Ndumia <ndegwandumia@gmail.com>
2022-10-21 10:47:27 +0300

    added store page

--- client/src/pages/StorePage.js
+++ client/src/pages/StorePage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {useParams } from 'react-router-dom';
 
 function StorePage() {
-  return (
-    <div>StorePage</div>
-  )
+    const params = useParams()
+    const storeId = params.storeId
+
+    const[mStore, setmStore] = useState()
+    let storesUrl = ""
+    //gets specific store using id
+    useEffect(() =>{
+         fetch(`${storesUrl}/${storeId}`)
+        .then(r => r.json())
+        .then(data => {
+            setmStore(data)
+
+        })
+        .catch((err)=>{
+            console.log(err.message);
+
+        });
+
+    },[storeId])
+
+    return (
+        <div>
+            {/* render report of store with store data*/}
+        </div>
+    );
 }
 
-export default StorePage
+export default StorePage;
