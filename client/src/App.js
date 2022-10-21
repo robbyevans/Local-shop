7b2d834414e0dac8dff0250d4842ea11f1c382e0
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-21 17:15:17 +0300

    modify landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
-//import logo from './logo.svg';
+
 import './App.css';
-//import ClerkPage from './ClerkPage';
-import Navbar from './Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import OrderForm from './pages/Order/OrderForm';
-import InventoryForm from './pages/Inventory/inventoryform';
+import Landing from './Mainpage/Landing';
+
 
 
 function App() {
   return (
-<>
-    <Router>
-    <Navbar />
-    <Switch>
-     <Route path='/' exact component={Home} />
-        <Route path='/orders' component={OrderForm} />
-        <Route path='/inventory' component={InventoryForm} />
-        </Switch>
-      </Router>
 
-      </>
+   
+<Landing />
+      
   );
 }
 
