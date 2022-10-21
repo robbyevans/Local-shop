dcbae111695c8e7bdb92a565d73cfaf1604ce5e1
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-21 22:09:38 +0300

    remove maibar css

--- client/src/MainLandingPage/Landing.jsx
+++ client/src/MainLandingPage/Landing.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+import '../'
+
 import MainBar from './MainBar/MainBar'
 import Footer from './footer/Footer'
 import Register from './RegisterMerchant/Register'
