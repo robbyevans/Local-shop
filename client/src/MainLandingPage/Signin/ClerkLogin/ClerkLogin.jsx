46c4188df7d06848c7aee658d5a70d8be54cee12
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-21 21:33:27 +0300

    replce clerk login component

--- client/src/MainLandingPage/Signin/ClerkLogin/ClerkLogin.jsx
+++ client/src/MainLandingPage/Signin/ClerkLogin/ClerkLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState }from 'react'
-import "../../design.css"
 
 const ClerkLogin = ({setUser}) => {
+
   const[username, setUsername] = useState("");
   const[password, setPassword] = useState("");
 
@@ -23,44 +23,28 @@ const ClerkLogin = ({setUser}) => {
 
   return (
     <>
-      <section className='forms top'>
-        <div className='container'>
-          <div className='sign-box'>
-            <p>Enter your username and password below to log in to your account and use the benefits of our website.</p>
-            <form action='' onSubmit={handleSubmit} >
-            <input type="text"
+      <section className='showcase login'>
+        <div className='showcase-overlay'>
+          <form className='form-control'>
+          <input type="text"
                 id="username"
+                placeholder='username'
                 autoComplete="off"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
           />
-              <input type="password"
+           <input type="password"
                 id="password"
                 autoComplete="current-password"
                 value={password}
+                placeholder='password'
                 onChange={(e) => setPassword(e.target.value)} 
           />
-
-              <div className='flex_space'>
-                <div className='flex'>
-                  <input type='checkbox' />
-                  <label>Remember Me</label>
-                </div>
-                <div className='flex'>
-                  <span>I forgot my password</span>
-                </div>
-              </div>
-
-              <button type='submit' className='primary-btn'>
-                Sign In
-              </button>
-            
-            </form>
-          </div>
+          
+            <button type='submit'>Log In</button>
+          </form>
         </div>
       </section>
-
-     
     </>
   )
 }
