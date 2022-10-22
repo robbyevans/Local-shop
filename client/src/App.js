a34279499b637b6c890b2af244f3c65fabce6933
Peter Ndumia <ndegwandumia@gmail.com>
2022-10-22 10:38:23 +0300

    merchant feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,11 @@
 import './App.css';
-import { useState, useCallback } from "react";
-import { Route, Routes } from "react-router";
-import MerchantPage from "./pages/MerchantPage";
-import StorePage from "./pages/StorePage";
- 
-
-
+import Merchant from './Merchant';
 function App() {
-  const [admins, setAdmins] = useState([]);
-  //mStores meaning merchant stores
-  const [mStores, setmStores] = useState([]);
 
-  const getAdmins = useCallback((admins) => {
-    setAdmins(admins);
-  }, []);
-
-  const getmStores = useCallback((mStores) => {
-    setmStores(mStores);
-  }, []);
-
-  function onAddAdmin(newAdmin) {
-    setAdmins([...admins, newAdmin]);
-  }
-  function onDeleteAdmin(deletedAdmin){
-    const newAdmins = admins.filter(admin=> admin.id!== deletedAdmin.id);
-    setAdmins(newAdmins)
-  }
 
   return (
-    <div>
-      <Routes>
-        <Route path="/merchantpage"element={<MerchantPage admins={admins} onDeleteAdmin={onDeleteAdmin} getAdmins={getAdmins} mStores={mStores} getmStores={getmStores} onAddAdmin={onAddAdmin}/>}></Route>
-        <Route path="/storepage/:storeId" element={<StorePage />}></Route>
-      </Routes>
+    <div className='App'>
+        <Merchant/>
     </div>
   );
 }
