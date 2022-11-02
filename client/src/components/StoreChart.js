0fe74fd428849ab3f06ed6e7261e0b56a59312e9
Evans Rutto <robbyevans001@gmail.com>
2022-11-02 10:36:43 +0300

    done

--- client/src/components/StoreChart.js
+++ client/src/components/StoreChart.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Bargraph from '../pages/Bargraph';
-// import Piechart from '../pages/Piechart';
+import Piechart from '../pages/Piechart';
+import Linegraph from '../pages/Linegraph';
 
 function StoreChart({storeId}) {
     return (
-        <div>
-            {/* <Piechart /> */}
-            <Bargraph storeId={storeId}/>
+        <div className='charts'>
+            {/* <div className="secondary-chart"> */}
+                {/* <Piechart storeId={storeId}/> */}
+            {/* </div> */}
+            {/* <div className="main-chart"> */}
+                <Bargraph storeId={storeId}/>
+                {/* <Linegraph storeId={storeId}/> */}
+            {/* </div> */}
         </div>
     );
 }
--- client/src/pages/Piechart.js
+++ client/src/pages/Piechart.js
@@ -1,49 +1,85 @@
-// import React, { PureComponent } from 'react';
-// import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+// import "./st.css";
+import {useState,useEffect} from "react"
+import React from "react";
+import { PieChart, Pie, Sector, Cell,ResponsiveContainer } from "recharts";
 
 
-// const data = [
-//   { name: 'Group A', value: 400 },
-//   { name: 'Group B', value: 300 },
-//   { name: 'Group C', value: 300 },
-//   { name: 'Group D', value: 200 },
-// ];
-// const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
-// export default function Piechart () {
-  
-//     return (
-//       <PieChart width={200} height={200} onMouseEnter={this.onPieEnter}>
-//         <Pie
-//           data={data}
-//           cx={120}
-//           cy={200}
-//           innerRadius={60}
-//           outerRadius={80}
-//           fill="#8884d8"
-//           paddingAngle={5}
-//           dataKey="value"
-//         >
-//           {data.map((entry, index) => (
-//             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-//           ))}
-//         </Pie>
-//         <Pie
-//           data={data}
-//           cx={420}
-//           cy={200}
-//           startAngle={180}
-//           endAngle={0}
-//           innerRadius={60}
-//           outerRadius={80}
-//           fill="#8884d8"
-//           paddingAngle={5}
-//           dataKey="value"
-//         >
-//           {data.map((entry, index) => (
-//             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-//           ))}
-//         </Pie>
-//       </PieChart>
-//     );
-// }
+export default function Piechart(storeId) {
+  const[data,setData]=useState([])
+  console.log(data)
+ 
+  useEffect(() => {
+    fetch(`/stores/${storeId.storeId}/items`).then((r) => {
+      if (r.ok) {
+        r.json().then((data) => setData(data));
+      }
+      else
+      console.log("NO RECORDS FOUND!")
+      console.log(storeId)
+    });
+  },[]);
+
+
+
+// label on individual chart
+  return (
+    <div className="piechart">
+     
+     <PieChart width={400} height={400}  >
+        <Pie
+          label={({
+            cx,
+            cy,
+            midAngle,
+            innerRadius,
+            outerRadius,
+            value,
+            index
+          }) => {
+            console.log("handling label?");
+            const RADIAN = Math.PI / 180;
+            // eslint-disable-next-line
+            const radius = 25 + innerRadius + (outerRadius - innerRadius);
+            // eslint-disable-next-line
+            const x = cx + radius * Math.cos(-midAngle * RADIAN);
+            // eslint-disable-next-line
+            const y = cy + radius * Math.sin(-midAngle * RADIAN);
+            return (
+              <text
+                x={x}
+                y={y}
+                fill="#8884d8"
+                textAnchor={x > cx ? "start" : "end"}
+                dominantBaseline="central"
+              >
+                {data[index].name} ({value})
+              </text>
+            );
+          }}
+          // end
+          // the  actual piechart
+          data={data}
+          cx={120}
+          cy={200}
+      
+          labelLine={true}
+      
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="quantity"
+      
+        >
+      
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      
+      </PieChart>
+    </div>
+  );
+}
